5fbbf372b4ffc0ec4cba59ef5f4bb02edb6f1ce6
Fallfromgrace18 <149462981+Fallfromgrace18@users.noreply.github.com>
2024-05-18 15:04:41 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solution here
+
+//You'll be writing three functions:
+
+//* `findMatching`- This function takes an array of drivers' names and a `string`
+  //as arguments, and returns the matching list of drivers. The function should be
+  //case insensitive.
+
+//* `fuzzyMatch` - This function takes an array of drivers' names and a `string`
+  //as arguments for querying the array, and returns all drivers whose names begin
+  //with the provided letters.
+
+//* `matchName` - This function takes an array of `driver` objects and a `string`
+  //as arguments. Each `driver` object has two properties: `name` and `hometown`.
+  //The function should return each element whose `name` property matches the
+ // provided `string` argument.
+
+//Remember the workflow:
+
+//1. Install the dependencies using `npm install`.
+//2. Run the tests using `npm test`.
+//3. Read the errors; vocalize what they're asking you to do.
+//4. Write code; repeat steps 2 and 3 often until a test passes.
+//5. Repeat as needed for the remaining tests.
+
+//After you have all the tests passing, remember to commit and push your changes
+//up to GitHub, then submit your work to Canvas using CodeGrade.
+
+//## Resources
+
+//* [MDN: Array.prototype.filter()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter)
+
+
+function findMatching(drivers, name) {
+    const result=drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase());
+    return(result)
+}
+
+function fuzzyMatch(drivers, string){
+    const result= drivers.filter((driverNames)=>driverNames.startsWith(string))
+        return(result)
+     }
+
+     function matchName(drivers,string){
+        const result =drivers.filter((driver)=>driver.name===string)
+        console.log("This is the name", result)
+        return (result)
+     
+        }
+     
+
